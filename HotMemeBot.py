8c801cf9c4c47c8830cde05c6ba61c9936c240d8
ajhein <ajhein@iu.edu>
2022-08-31 18:31:33 -0700

    first commit

---
+++ HotMemeBot.py
@@ -0,0 +1,8 @@
+import Tweepy, time
+
+
+## Create a twitter bot that will post a random meme from a youtube playlist every 24 hours
+
+# Create a twitter account and get the API keys from https://developer.twitter.com/en/apps
+# Create a youtube playlist and get the playlist ID from the URL
+
